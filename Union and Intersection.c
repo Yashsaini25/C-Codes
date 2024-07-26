33b293d3735c37e85a3a5909a6b95c76b4488d02
Yash Saini <160393706+Yashsaini25@users.noreply.github.com>
2024-07-26 18:43:34 +0530

    Union and Intersection of elements of arrays.
    Union and Intersection of elements of arrays using dynamic memory allocation.

---
+++ Union and Intersection.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int j,p,l=0,k,*a,*b,*u,*i,n,m;
+    printf("Enter the no. of elements for 1st array: ");
+    scanf("%d",&n);
+    printf("Enter the no. of elements for 2nd array: ");
+    scanf("%d",&m);
+    a=(int*)malloc(n*sizeof(int));
+    b=(int*)malloc(m*sizeof(int));
+    u=(int*)malloc((m+n)*sizeof(int));
+    i=(int*)malloc(n*sizeof(int));
+    printf("Enter %d elements for 1st array:\n",n);
+    for(j=0;j<n;j++)
+    {
+     scanf("%d",&a[j]);
+     *(u+j)=a[j];
+    }
+    printf("Enter %d elements for 2nd array:\n",m);
+    for(j=0;j<m;j++)
+    scanf("%d",&b[j]);
+    p=n;
+    for(j=0;j<m;j++)
+    {
+        for(k=0;k<n;k++)
+        {
+            if(*(b+j)==*(a+k))
+            {
+                l++;
+                break;
+            }
+        }
+        if(l==0)
+        {
+            *(u+p)=*(b+j);
+            p++;
+        }
+        l=0;
+    }
+    l=0;
+    for(j=0;j<n;j++)
+    {
+        for(k=0;k<m;k++)
+        {
+            if(*(a+j)==(*(b+k)))
+            {
+                *(i+l)=*(a+j);
+                l++;
+                break;
+            }
+        }
+    }
+    printf("Union of elements: ");
+    for(j=0;j<p;j++)
+    printf(" %d",*(u+j));
+    printf("\n");
+    printf("Intersection of elements: ");
+    for(j=0;j<l;j++)
+    printf(" %d",*(i+j));
+    printf("\n");
+    return 0;
+
+}
